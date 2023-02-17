de85102d0885907328496f83418577acc5ea9a29
earth-north <earth-north@163.com>
2023-02-17 10:38:57 +0800

    修正代码

---
+++ README.md
@@ -0,0 +1,6 @@
+```shell
+python.exe -m pip install --upgrade pip
+pip install m3u8
+pip install pycryptodome
+pip install requests
+```
--- m3u8demo.py
+++ m3u8demo.py
@@ -8,16 +8,20 @@ import os
 
 # 动态区域
 url_list=[
-    'http://hls.videocc.net/b034527feb/f/b034527feb9a9b1c98d71611dbdcc77f_3.m3u8?pid=1676558867582X1285569&device=desktop',
+    'http://hls.videocc.net/b034527feb/9/b034527febcdbb20f59d3072c3f41849_2.m3u8?pid=1676599967241X1691046&device=desktop',
+    'http://hls.videocc.net/b034527feb/e/b034527feb1822e8f5eb8b595806392e_2.m3u8?pid=1676600013119X1013391&device=desktop',
 ]#在这里天上你要下载的m3u8文件的URL
 token_list=[
-    '?token=bcd67f71-40d7-4c18-bca1-b34ffff45aa2-80',
+    '?token=a3b2ed1c-10a0-4c56-a571-7b69b3a6732e-j0',
+    '?token=8f3a8830-50f5-483e-a037-c03a24a34b05-60',
 ]
 path_list=[
-    '高数/第一章 极限和连续(2022)/考点三 连续/',
+    '高数/第二章 一元函数微分学(2022)/考点一 导数与微分/',
+    '高数/第二章 一元函数微分学(2022)/考点一 导数与微分/',
 ]
 title_list=[
-    '连续',
+    '导数与微分（上）',
+    '导数与微分（下）',
 ]
 # 动态区域
 
